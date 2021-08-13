91133d42dfeae8ed73cae591534f4da1665dbf0e
wihi48 <wihi48@Aiman-Lappy>
2021-08-13 10:15:27 +0800

    Edit  Home Page After This

--- post.php
+++ post.php
@@ -100,6 +100,9 @@ $avg = $total / $numR;
             }
 
         </script>
+        <!--Comment System using PHP and Ajax-->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
     </head>
     <body>
         <?php
@@ -226,9 +229,29 @@ $avg = $total / $numR;
                             <br><br>
                             <?php echo round($avg, 2) ?>
                         </div>
-
                     </div>
                 </div>
+            </div>
+            <!--Comment Section-->
+            <br />
+            <h2 align="center">Discussion Board</h2> <hr>
+            <br />
+            <div class="container">
+                <form method="POST" id="comment_form">
+                    <div class="form-group">
+                        <input type="text" name="comment_name" id="comment_name" class="form-control" placeholder="Enter Name" />
+                    </div> <br>
+                    <div class="form-group">
+                        <textarea name="comment_content" id="comment_content" class="form-control" placeholder="Enter Comment" rows="5"></textarea>
+                    </div> <br>
+                    <div class="form-group">
+                        <input type="hidden" name="comment_id" id="comment_id" value="0" />
+                        <input type="submit" name="submit" id="submit" class="btn btn-info" value="Submit" />
+                    </div>
+                </form>
+                <span id="comment_message"></span>
+                <br />
+                <div id="display_comment"></div>
             </div>
         </article>
         <!-- Footer-->
@@ -264,3 +287,51 @@ $avg = $total / $numR;
         <script src="js/scripts.js"></script>
     </body>
 </html>
+
+<!--Comment Section Stuff-->
+<script>
+            $(document).ready(function () {
+
+                $('#comment_form').on('submit', function (event) {
+                    event.preventDefault();
+                    var form_data = $(this).serialize();
+                    $.ajax({
+                        url: "add_comment.php",
+                        method: "POST",
+                        data: form_data,
+                        dataType: "JSON",
+                        success: function (data)
+                        {
+                            if (data.error != '')
+                            {
+                                $('#comment_form')[0].reset();
+                                $('#comment_message').html(data.error);
+                                $('#comment_id').val('0');
+                                load_comment();
+                            }
+                        }
+                    })
+                });
+
+                load_comment();
+
+                function load_comment()
+                {
+                    $.ajax({
+                        url: "fetch_comment.php",
+                        method: "POST",
+                        success: function (data)
+                        {
+                            $('#display_comment').html(data);
+                        }
+                    })
+                }
+
+                $(document).on('click', '.reply', function () {
+                    var comment_id = $(this).attr("id");
+                    $('#comment_id').val(comment_id);
+                    $('#comment_name').focus();
+                });
+
+            });
+</script>
